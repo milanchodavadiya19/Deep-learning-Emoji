9e3609d9b5037d2b2db88977704a18b29d99222e
Milan Chodavadiya <120493459+milanchodavadiya19@users.noreply.github.com>
2023-07-30 22:30:53 +0200

    Update emoji.py

--- emoji.py
+++ emoji.py
@@ -13,6 +13,7 @@ from keras.layers import MaxPooling2D
 from keras.preprocessing.image import ImageDataGenerator
 import threading
 
+
 emotion_model = Sequential()
 emotion_model.add(Conv2D(32, kernel_size=(3,3), activation='relu', input_shape=(48,48,1)))
 emotion_model.add(Conv2D(32, kernel_size=(3,3), activation='relu'))
